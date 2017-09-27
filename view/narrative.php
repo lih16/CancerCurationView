0c45bcfb32a34463dcb17ddf26d26b30a2928980
Li <hli3302@gmail.com>
2017-09-27 12:03:44 -0400

    aws version

---
+++ view/narrative.php
@@ -0,0 +1,36 @@
+<div id="nardiv" >
+  
+</div>
+<div id="editoriv" >
+ 
+</div>
+<div id="showedit" style="display:none;"></div>
+<table border=1>
+<tr><td valign="top">
+
+<div id="adminModify" ></div>
+</td>
+<td valign="top">
+<div id="versionlist" style="display:none;">
+	<table id="narrativelist"  cellspacing="0" width="100%">
+		<!--,'AssignID','Cancer','Gene','ProteinChange','Assign_Date','Due_Date','Save_Date','Submit_Date','Approved_Date','Status','Status'-->
+			<thead>
+				<tr>
+					
+					<th align="left">narrative</th>
+					<th align="left">ver_name</th>
+					<th align="left">date_admin</th>
+					
+				
+					
+				</tr>
+			</thead>
+		</table>
+		</div>
+		
+		</td></tr>
+		</table>
+<button class="searchbutton" id="adminEditB" onclick="modifyparagraph(event,'ll','33','44');">Edit</button>
+
+<button class="searchbutton" id="adminSaveB" onclick="adminSave(event,'ll','33','44');">Save</button>
+<button class="searchbutton" id="adminNewB" onclick="adminNewVersion(event,'ll','33','44');">New Version</button>
