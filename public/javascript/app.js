25bdf05e863bf0bbbc10e783ac3306406740075c
Ubuntu <ubuntu@ip-172-31-93-167.ec2.internal>
2017-11-10 20:03:10 +0000

    reverted back to original app.js and model_home

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -15,63 +15,61 @@ $(document).ready(function() {
         addMutationList(tissue, genename);
 
     });
-
+	
 	if(admin==1){
 		$("#adminEditB").text("All Comments").hide();
 	    $("#adminSaveB").hide();
 		$("#adminNewB").hide();
 	}
 	else{
-
+		
 		$("#adminEditB").text("Edit").hide();
 		$("#adminSaveB").hide();
 		$("#adminNewB").hide();
-
-
+		
+		
 	}
 
 });
-var greport=0;
 
-function narrative(e, tumor, gene, mutation,report) {
-	  greport=report
-
+function narrative(e, tumor, gene, mutation) {
+	
     e.preventDefault();
-
+	
     gtissue   = $("#tumorTypeselect option:selected").text();
-
+	
     ggene     = $("#geneselect option:selected").text();
     gmutation = $("#mutationselect option:selected").text();
 	if(gtissue.indexOf("select")>0){
-
+		
 		alert("please select Tumor  first");
 		return false;
-
+		
 	}
 	if(ggene.indexOf("select")>0){
-
+		
 		alert("please select gene name first");
 		return false;
-
+		
 	}
 	if(gmutation.indexOf("select")>0){
-
+		
 		alert("please select alteration first");
 		return false;
-
+		
 	}
-
+	
 	$(editdiv).hide();
 	startWorker();
-
-
+	
+	
     var ret=getnarrative("tissue");
-
-
+	
+	
 }
 
 function getnarrative(tissue1) {
-
+	
     $.ajax({
 		async:false,
         type: 'POST',
@@ -84,7 +82,7 @@ function getnarrative(tissue1) {
 			variant: gmutation
         },
         success: function(data1) {
-
+			
 			if((data1=="1")||(!data1)){
 				alert("There is no narrative yet");
 				if(admin==1){
@@ -95,19 +93,19 @@ function getnarrative(tissue1) {
 		$("#adminNewB").hide();
 	}
 	else{
-
+		
 		$("#adminEditB").text("Edit").hide();
 		$("#adminSaveB").hide();
 		$("#adminNewB").hide();
-
-
+		
+		
 	}
 	$("#versionlist").hide();
 	$("#nardiv").hide();
 				return false;
-
+				
 			}
-
+			
 			if(data1){
               $("#nardiv").html(data1);
 			 // $("#nardiv").show();
@@ -120,18 +118,18 @@ function getnarrative(tissue1) {
 		$("#adminNewB").show();
 	}
 	else{
-
+		
 		$("#adminEditB").text("Edit").hide();
 		$("#adminSaveB").hide();
 		$("#adminNewB").hide();
-
-
+		
+		
 	}
 	$("#nardiv").show();
 	loadnarrativeTable();
 	$("#versionlist").show();
 			}
-
+			
             return false;
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -225,7 +223,6 @@ function save_comment_paragrah(pid, comment) {
             gene: ggene,
             mutation: gmutation,
             pid: pid,
-						report:greport
 			version:gcurVername,
             comment: comment,
             uid: uid
@@ -248,7 +245,7 @@ function modifycomment(e, id, index, status) {
 	if (status ==3){
 		$(id).closest('p').find('textarea').hide();
 		return false;
-
+		
 	}
     if ( ! $(id).closest('p').find('textarea').is(':visible')) {
         $(id).closest('p').find('textarea').show();
@@ -256,9 +253,9 @@ function modifycomment(e, id, index, status) {
 		$(id).text("save");
 		gstatus=1;
         return false;;
-    }
+    } 
 	else { //save the narrative to the database.
-
+	   
         var comment = $(id).closest('p').find('textarea').val();
 		if (!$.trim(comment)) {
            alert("Please input your comment;");
@@ -271,7 +268,7 @@ function modifycomment(e, id, index, status) {
         save_comment_paragrah(index, comment);
         return false;
     }
-
+	
 
 }
 var curdiv = "#nardiv";
@@ -302,9 +299,9 @@ function modifyparagraph(e, cancertype, gene, mutation) {
            return false;
        }
         $(editdiv).show();
-
+		
 		$(editdiv).html($("#nardiv").html());
-
+		
         $(editdiv).find("p").each(function(index) {
             index = index + 1;
             var divarea = "<div class=\"divcomment\" >sdafasdfsadf</div>";
@@ -318,11 +315,11 @@ function modifyparagraph(e, cancertype, gene, mutation) {
 				loadnarrativeTable();
 			}
 			else{
-
+				
 				$(this).html(cindex + " "  + $(this).html() +  divarea);
 				$("#versionlist").show();
 				loadnarrativeTable();
-
+				 
 			}
         });
         updateMsg();
@@ -330,16 +327,16 @@ function modifyparagraph(e, cancertype, gene, mutation) {
 function render(id,data){
 	var html="<ul>";
    $.each(data, function(i, item) {
-    var colori=colorCode[item.uid];
-
-
+    var colori=colorCode[item.uid]; 
+   	
+	
     html=html+"<li><span style=\"color:"+colori+"\">"+item.uid+": "+item.date_edit+": "+item.comment+"</span></li>";
-
-
+   
+                
   });
   html=html+"</ul>";
   id.html(html);
-}
+}	
 var colorCode={};
 var colorArray=[];
 var num_colors=100;
@@ -350,7 +347,7 @@ function randomVal(min, max) {
   return Math.floor(Math.random() * (max - min) + 1) + min;
 }
 
-/* TO CUSTOMIZE
+/* TO CUSTOMIZE 
 
 In the generate() function below, change the numbers in randomVal(); min to max
 
@@ -368,7 +365,7 @@ for (var i = 0; i < num_colors; i += 1) {
 	h=Math.floor(Math.random() * num_colors) * (360 / num_colors) % 360 ;//* (360 / num_colors) % 360;//randomVal(0, 360);
 	s=1.0;//randomVal(30, 95);
 	l=0.5;//randomVal(30, 80);
-
+	
 	var rgb=hslToRgb(h, s, l);
 	var hex=rgbToHex(rgb[0], rgb[1], rgb[2]);
 	alert(hex);
@@ -377,8 +374,8 @@ for (var i = 0; i < num_colors; i += 1) {
 	//alert(color);
 	colorArray.push(color);
 }
-
-
+	
+	
 
 
 /**
@@ -433,23 +430,22 @@ function getmessage(pid, id) {
             gene: ggene,
             mutation: gmutation,
             pid: pid,
-						report:greport
         },
         success: function(data1) {
 			var i=0;
 			var uid1;
 			$.each(data1, function(key1, value1) {
 				//
-
+				
 				colorCode[value1.uid]=colorArray[key1];
-
-
+				
+  
            });
             render(id,data1);
             return false;
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-
+            
             return false;
         }
     });
@@ -465,7 +461,6 @@ function getAjaxMessage() {
             cancer: gtissue,
             gene: ggene,
             mutation: gmutation,
-						report:greport
 			version: gcurVername
         },
         success: function(data1) {
@@ -484,7 +479,7 @@ function getAjaxMessage() {
 				if(typeof colorCode[item.uid] === "undefined"){
 					colorCode[item.uid]=uidindex;
 					uidindex=uidindex+1;
-
+					
 				}
 				colori=cols[colorCode[item.uid]];
 				if (typeof commentObj[item.paragraph_id] === "undefined") {
@@ -494,14 +489,14 @@ function getAjaxMessage() {
 				commentObj[item.paragraph_id]=commentObj[item.paragraph_id]+"<li><span style=\"color:"+colori+"\">"+item.uid+": "+item.date_edit+": "+item.comment+"</span></li>";
 				}
 				//alert(commentObj[item.paragraph_id]);
-
+				
 			}
 			addMessage(commentObj);
-
+			
             return false;
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-
+            
             return false;
         }
     });
@@ -524,9 +519,9 @@ function addMessage(obj) {
 	   else{
 		   $(id).html("<ul>"+obj[index]+"</ul>");
 	   }
-
-
-
+	
+  
+  
     });
 
 }
@@ -552,7 +547,7 @@ function getComment(){
 				if(typeof colorCode[item.uid] === "undefined"){
 					colorCode[item.uid]=uidindex;
 					uidindex=uidindex+1;
-
+					
 				}
 				colori=cols[colorCode[item.uid]];
 				if (typeof commentObj[item.paragraph_id] === "undefined") {
@@ -562,17 +557,17 @@ function getComment(){
 				commentObj[item.paragraph_id]=commentObj[item.paragraph_id]+"<li><span style=\"color:"+colori+"\">"+item.uid+": "+item.date_edit+": "+item.comment+"</span></li>";
 				}
 				//alert(commentObj[item.paragraph_id]);
-
+				
 			}
 			addMessage(commentObj);
 			 //w.terminate();
-
+			
 	};
 }
 function updateMsg() {
   //  addMessage();
 	//if(admin==2)
-   // setTimeout('updateMsg()', 1400);
+   // setTimeout('updateMsg()', 1400); 
 }
 function generateHtml(htmlcontent){
 	var mtext = "";
@@ -597,7 +592,7 @@ function generateHtml(htmlcontent){
 
    /* $(editdiv).find("p").each(function(index) {
           index = index + 1;
-
+		 
           html = html + "paragraph " + index + ":<span class=\"notin\" style=\"color:red\">" + $(this).find('.divcomment').html() + "</span><br><hr>";
 
      });
@@ -614,20 +609,20 @@ function generateHtml(htmlcontent){
      $("#adminModify").html(text1 + "<div style=\"border-style: dotted;border-width: 2px;\">" + html + "</div>");*/
      //$(curdiv).html(curdivclone.html());
      //$(curdiv).hide();
-
+	
 }
 function adminmodify(e, stu,id) {
-
-
+	
+	
 	 e.preventDefault();
 	 changeColor();
 	 $(id).css('color','red');
 	 $(id).text("Current Version");
-
+	
 	 if(stu==2){
-
+		 
 		 generateHtml(2);
-
+		 
 	 }
 	 else{
 	   var myhtml=$(id).closest('td').find('.hidediv').html();
@@ -635,7 +630,7 @@ function adminmodify(e, stu,id) {
        generateHtml(myhtml);
 	   //alert($(id).closest('tr').find('td').eq(1).html());
 	   gcurVername=$(id).closest('tr').find('td').eq(1).html();
-
+	   
 	 }
 	 //alert(gcurVername);
 	// getComment();
@@ -646,7 +641,7 @@ function adminmodify(e, stu,id) {
 	 return false;
 }
 function getnarrativeList() {
-
+	
     $.ajax({
         type: 'POST',
         url: 'getnarrativeList',
@@ -655,7 +650,6 @@ function getnarrativeList() {
             cancer: gtissue,
             gene: ggene,
             mutation: gmutation
-						report:greport
 
         },
         success: function(data1) {
@@ -688,13 +682,13 @@ function loadnarrativeTable() {
         "ajax": {
             "url": "getnarrativeList?gene="+ggene+"&cancer="+gtissue+"&variant="+gmutation,
             "type": "GET"
-
+       
         }
     });
     if (n == 1)
         table.fnDraw();
     return false;
-
+  
 
 }
 function changeColor(){
@@ -705,10 +699,10 @@ function changeColor(){
 		objcount.find('a').text("view");
 	   else
 		   objcount.find('a').text("modify");
-
-
+		   
+		
 	});
-
+	
 }
 
 function addnarButton() {
@@ -728,30 +722,30 @@ function addnarButton() {
 		$('#narrativelist > tbody tr').each(function(index, value) {
 			var objcount = $(this).find('td').eq(0);
 			//var hidedivHtml="<div class=\"hidediv\">"+objcount.html()+"</div><a href=\"#\" onclick=\"adminmodify(event, 1,this);return false;\">modify</a>";
-
+			
 			objcount.html(buttonHtml);
 		});
-
+		
 	}
 	else{
 		//gcurVername=1;
 		$('#narrativelist > tbody tr').each(function(index, value) {
 			var objcount = $(this).find('td').eq(0);
-
+		
 	  var name="modify";
 	  var color="blue";
 	  if(admin==2){
 		  name="view";
-
+		  
 	  }
-
+	 
 	 if(gcurVername==$(this).find('td').eq(1).html()){
 		 name="Current Version";
 		 color="red"
 	 }
-
+	
 			var hidedivHtml="<div class=\"hidediv\">"+objcount.html()+"</div><a href=\"#\" style=\"color:"+color+"\" onclick=\"adminmodify(event, 1,this);return false;\">"+name+"</a>";
-
+			
 			objcount.html(hidedivHtml);
 		});
 	}
@@ -762,18 +756,18 @@ function saveNarrative(e,saveOrnot) {
 		//alert(saveOrnot+":"+mynarrative);
 		var version;
 		if(saveOrnot==0){
-
+		   
 		   if( $("#newvInput").val().length === 0 ){
 			   alert("Please select your current version first!");
 			   return false;
-
+			   
 		   }
 		   else{
 			   version=$("#newvInput").val();
 		   }
 		}
-		else{
-
+		else{ 
+		   
 			version=gcurVername;
 			// alert(version);
 		}
@@ -786,13 +780,12 @@ function saveNarrative(e,saveOrnot) {
                 cancer:   gtissue,
                 gene:     ggene,
                 mutation: gmutation,
-								getnarrativeList
 				narrative:mynarrative,
 				ver_name: version,
 				saveormodify:saveOrnot
             },
             success: function(data1) {
-
+				
 				loadnarrativeTable();
 				//alert("Your narrative has been stored successfully!");
                 return false;
@@ -805,7 +798,7 @@ function saveNarrative(e,saveOrnot) {
 
 
  }
-
+  
 function adminNewVersion(e, cancer, gene, mutation) {
 	e.preventDefault();
     openDialog();
@@ -818,15 +811,15 @@ function closeNewVdialog(e, saveOrnot) {
 	$("#newvDialog").dialog("close");
     if (saveOrnot == 0) {
 		saveNarrative(e,0);
-
-    }
+        
+    } 
 }
 function  openDialog()
 {
     var dt = new Date();
     var time = "version_" + dt.getFullYear() +"_"+(dt.getMonth()+1) +"_"+dt.getDate() +"_"+dt.getHours() + "_" + dt.getMinutes() + "_" + dt.getSeconds();
     $("#newvInput").val(time);
-    //
+    // 
     $("#newvDialog").dialog({
         autoOpen: true,
         hide: "puff",
@@ -842,7 +835,7 @@ function adminSave(e, cancertype, gene, mutation) {
 	else{
 		saveNarrative(e,1);
 	}
-
+ 
 }
 function showAnnotation(){
 	//alert("aavv");
@@ -854,5 +847,5 @@ function showAnnotation(){
 	window.open(url, 'window name', 'window settings')
    // window.location.href="https://lih16.u.hpc.mssm.edu/pipeline/js/cancerVariantCuration/CancerVarCuation_forViewer.php?cancer="+gtissue+"&gene="+ggene+"&mutation="+gmutation;
 	//window.location.href ="https://www.google.com";
-
+	
 }
