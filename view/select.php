3623a4ab5f49f765f08a13c006b3b135f044896d
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-11-30 14:53:10 -0500

    test

--- view/select.php
+++ view/select.php
@@ -85,6 +85,8 @@ function myFunction() {
 &nbsp;&nbsp;&nbsp;&nbsp;
 <button class="searchbutton"  onclick="showAnnotation();return false;">Alteration Information</button>
 &nbsp;&nbsp;&nbsp;&nbsp;
+<button onclick="myFunction()">Narrative Key</button>
+&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="http://34.235.93.148/CancerCurationView/public/images/LevelsOfEvidence.pdf"   target="_blank" style="background-color: #8FBE00; border-radius: 5px 5px 5px 5px; color: white; font-family: Enriqueta,Arial,sans-serif; text-transform: uppercase; font-size: 16px; padding: 3px 10px;">OncoKB Guideline</a>
 </form>
 </div>
