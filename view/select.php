2a9842a8743a7bca1e65e5aff1e5a83ef1c8d2c4
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-11-30 12:38:29 -0500

    test

--- view/select.php
+++ view/select.php
@@ -20,66 +20,23 @@
 
   <option value="1" selected>Alterations</option>
 </select>
-<br>
-<table align=center border=1 cellpadding="8">
-	<style>
-	table.steelBlueCols {
-  border: 4px solid #555555;
-  background-color: #555555;
-  width: 400px;
-  text-align: center;
-  border-collapse: collapse;
-}
-table.steelBlueCols td, table.steelBlueCols th {
-  border: 1px solid #555555;
-  padding: 5px 10px;
-}
-table.steelBlueCols tbody td {
-  font-size: 12px;
-  font-weight: bold;
-  color: #FFFFFF;
-}
-table.steelBlueCols td:nth-child(even) {
-  background: #398AA4;
-}
-table.steelBlueCols thead {
-  background: #398AA4;
-  border-bottom: 10px solid #398AA4;
-}
-table.steelBlueCols thead th {
-  font-size: 15px;
-  font-weight: bold;
-  color: #FFFFFF;
-  text-align: left;
-  border-left: 2px solid #398AA4;
-}
-table.steelBlueCols thead th:first-child {
-  border-left: none;
-}
+<br clear='both'>
 
-table.steelBlueCols tfoot td {
-  font-size: 13px;
-}
-table.steelBlueCols tfoot .links {
-  text-align: right;
-}
-table.steelBlueCols tfoot .links a{
-  display: inline-block;
-  background: #FFFFFF;
-  color: #398AA4;
-  padding: 2px 8px;
-  border-radius: 5px;
-}
-	</style>
+<table align=center border=1 cellpadding="8">
+
 <thead>
 <tr>
-<th>Key </th>
-<th>None: &#x25CB;</th>
-<th>Narrative: &#x25D0; </th>
-<th>Report-Style: &#x25D1;</th>
-<th>Both &#x25CF;</th>
-</tr>
+<th colspan=4 >Key for indentify which alterations have Narrative </th>
+  </tr>
 </thead>
+<tbody>
+  <tr>
+<td>None: &#x25CB;</td>
+<td>Narrative: &#x25D0; </td>
+<td>Report-Style: &#x25D1;</td>
+<td>Both: &#x25CF;</td>
+    </tr>
+</tbody>
 </table>
 <br>
 <button class="searchbutton"  onclick="narrative(event,'ll','33','44',0);">Narrative</button>
