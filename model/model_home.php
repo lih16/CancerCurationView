06de342cf5160296a3d3483061da0001095af7b3
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-12-01 14:46:37 -0500

    test

--- model/model_home.php
+++ model/model_home.php
@@ -2,7 +2,7 @@
 
 use Lib\model_base;
 $logpath= "/hpc/users/siddio01/www/Development/";
-$logfile='/hpc/users/siddio01/www/Development/report.txt';
+$logfile='report.txt';
 file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
 class Tumor_Model extends model_base
 {
@@ -96,10 +96,10 @@ class Tumor_Model extends model_base
         //$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
         if ($report == 1) {
             $sQuery = "select narrative from kb_CancerVariant_Curation.CVC_viewer_admin_report_aws where gene = '" . $gene . "' and variant = '" . $variant . "'  and cancer = '" . $cancer . "'  order by date_admin desc limit 1";
-            file_put_contents($logfile, $sQuery, true );
+            file_put_contents('hpc/users/siddio01/www/Development/report.txt', $sQuery, true);
         } else {
             $sQuery = "select narrative from kb_CancerVariant_Curation.CVC_viewer_admin_aws where gene = '" . $gene . "' and variant = '" . $variant . "'  and cancer = '" . $cancer . "'  order by date_admin desc limit 1";
-            file_put_contents($logfile, $sQuery, FILE_APPEND );
+            file_put_contents('hpc/users/siddio01/www/Development/report.txt', $sQuery, FILE_APPEND );
         }
         //$stmt     = $this->db->prepare($sQuery);
         //echo $sQuery;
