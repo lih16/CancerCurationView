67b95abc207e75830f630e9741d25319c14467af
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-12-15 16:13:02 -0500

    changed the ==

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -211,7 +211,7 @@ function constructHtml(groupObj) {
 
         for (var i = 0; i < mutations.length; i++) {
             var mutations_w = notifyNarrativeTable(mutations[i]);
-             if (mutations_w != "parse error"){
+             if (mutations_w == "parse error"){
                continue;
              }
             constructHtml = constructHtml + "<option>";
