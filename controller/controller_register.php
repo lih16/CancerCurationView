ec020f516f575e1b5d33760229268aa0cfb5458b
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-12-19 13:49:50 -0500

    controller register

---
+++ controller/controller_register.php
@@ -0,0 +1,35 @@
+<?php
+//namespace Controller;
+
+use Lib\model_base;
+
+use Lib\Controller_base;
+
+class controller_register extends Controller_base
+{
+  /**
+   * Create the Controller model
+   *  @param  $model Designate a place to hold class dependencies
+   */
+    public $model;
+    public function __construct()
+    {
+      /**
+       * Accept a $model instance in the constructor, so the  dependencies can be injected from the outside
+       * Set the dependency in a class property, so it's easily accessible for later use of class methods.
+       * @param $this means "this instance of class A"
+        * $this->model means "this instance of class Login_Model $model property
+       */
+      //  $this->model = new Register_Model();
+    }
+
+    public function action_register()
+    {
+      /**
+       * Set the dependency in a class property, so it's easily accessible for later use of class methods.
+        *@param $reslt calls the getlogin() function of model class and store the return value of this function into the reslt variable.
+    */
+            include VIEW_PATH . 'register.php';
+    }
+
+}
