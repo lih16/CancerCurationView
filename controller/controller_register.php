9053ac858f6cf9992b95a4388a572429a11d620f
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-12-21 09:37:21 -0500

    fixed error

--- controller/controller_register.php
+++ controller/controller_register.php
@@ -41,7 +41,7 @@ class controller_register extends Controller_base
           if ($reslt == '1'){
               include VIEW_PATH . 'login.php';
           } else {
-              iinclude VIEW_PATH . 'failure.php';
+              include VIEW_PATH . 'failure.php';
           }
 
     }
