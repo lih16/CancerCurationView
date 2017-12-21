d1723864bb4498c35f6354f0fd0405e23d26bf97
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-12-21 09:43:57 -0500

    added model_register

--- model/model_register.php
+++ model/model_register.php
@@ -3,12 +3,43 @@
 use Lib\model_base;
 
 class Login_Model extends model_base
-{  public function __construct($stable = null, $aColumns = null, $sIndexColumn = null)
+{
+  public function __construct($stable = null, $aColumns = null, $sIndexColumn = null)
   {
       parent::__construct(null, null, null);
   }
+  public function adduser($username,$pass,$verifynumber){
+      //copy v$report = $_POST["report"];
+      $this->db = Db::getInstance();
+      if ($report == 1) {
+          $stmt = $this->db->prepare("INSERT INTO CVC_User (UID, Name,Email,1,Password) VALUES (:cancer, :gene,:varaint,:pid,:uid,:date_edit,:comment,:version_name)");
+      } else {
+          $stmt = $this->db->prepare("INSERT INTO CVC_User (cancer, gene,varaint,1,uid,date_edit,comment,version_name) VALUES (:cancer, :gene,:varaint,:pid,:uid,:date_edit,:comment,:version_name)");
+      }
+      $stmt->bindParam(':UID', $email);
+      $stmt->bindParam(':Name', $name);
+      $stmt->bindParam(':Email', $email);
+      $stmt->bindParam(':Role', $role);
+      $stmt->bindParam(':Password', $password);
 
-  public function getlogin()
+      $email = $_POST["email"];
+      $name = $_POST["name"];
+      $role = $_POST["1"];
+      $password = $_POST["password"];
+
+      try {
+          $stmt->execute();
+          return 2;
+      } catch (PDOException $e) {
+          //write_log($e->getMessage());
+          echo $e->getMessage();
+          return $e->getMessage();
+      }
+
+  }
+
+
+  public function getRegister()
   {
       session_start(); // Starting Session
 
@@ -36,23 +67,21 @@ class Login_Model extends model_base
               $num_rows = count($rows);
 
               if ($num_rows == 1) {
-                  //$_SESSION['login_user']=$username; // Initializing Session
-                  $_SESSION['uname'] = $rows[0][0];
 
-                  $_SESSION['username'] = $user;
-                  $_SESSION['role'] = $role;
                   // header("location: profile.php"); // Redirecting To Other Page
-                  return 'login';
+                  return '1';//indicated that user alreaday exist;
                   //$_SESSION['login_user']=$username; // Initializing Session
                   // header("location: profile.php"); // Redirecting To Other Page
               } else {
-                  return 'invalid user';
+                    return dduser($username,$pass,$verifynumber);
+                  }
+                  // successfully add uesr
                   //$error = "Username or Password is invalid";
-              }
+
               //mysql_close($connection); // Closing Connection
           }
       }
-  }
 
+}
 }
 ?>
