22245ae74f524e1961bd829307413047f58d386e
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-12-22 15:29:24 -0500

    fixed failure page

--- view/failure.php
+++ view/failure.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
 <html>
-<header><title>RegistrationFAILURE</title></header>
+<head>
+  <title>Registration Failed</title>
+  <meta name="robots" content="noindex, nofollow">
+  <link href="<?php echo CSS_PATH; ?>/register.css" rel="stylesheet" type="text/css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+</head>
 <body>
-Registration failed,  username already exists.
+  <div class="container">
+    <div class="main">
+      <form action="http://oncsuite-cav.sema4genomics.com/">
+        <h2> Registration failed,  username already exists.</h2>
+      <button type="submit">Return to login page</button>
+      </form>
+    </div>
 </body>
 </html>
--- view/successLogin.php
+++ view/successLogin.php
@@ -1,26 +1,18 @@
 <!DOCTYPE html>
 <html>
-
 <head>
   <title>Thank you for registering</title>
   <meta name="robots" content="noindex, nofollow">
   <link href="<?php echo CSS_PATH; ?>/register.css" rel="stylesheet" type="text/css">
-
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-
-
 </head>
-
 <body>
   <div class="container">
     <div class="main">
       <form action="http://oncsuite-cav.sema4genomics.com/">
         <h2> You have successfully registered</h2>
-        <input type="button" onclick="location.href='http://oncsuite-cav.sema4genomics.com/';" value="Go to Google" />
-
       <button type="submit">Return to login page</button>
       </form>
     </div>
 </body>
-
 </html>
