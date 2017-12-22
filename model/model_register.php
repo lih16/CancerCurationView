ad392979539bf4b70b72aee465d2ca0f5a7e20f2
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-12-22 11:48:50 -0500

    changed username to email

--- model/model_register.php
+++ model/model_register.php
@@ -81,7 +81,7 @@ class Register_Model extends model_base
               $user = $_POST['email'];
               $pass = $_POST['password'];//
               // the message
-              $msg = $username +"has registered on cancer alteration viewer";
+              $msg = ['email'] +"has registered on cancer alteration viewer";
 
               // use wordwrap() if lines are longer than 70 characters
               $msg = wordwrap($msg,70);
