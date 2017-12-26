ed5ebd5a75f6a144333ea8010e890b8f65234271
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2017-12-26 14:54:14 -0500

    changed table border to 0

--- view/narrative.php
+++ view/narrative.php
@@ -1,11 +1,11 @@
 <div id="nardiv" >
-  
+
 </div>
 <div id="editoriv" >
- 
+
 </div>
 <div id="showedit" style="display:none;"></div>
-<table border=1>
+<table border=0>
 <tr><td valign="top">
 
 <div id="adminModify" ></div>
@@ -16,18 +16,18 @@
 		<!--,'AssignID','Cancer','Gene','ProteinChange','Assign_Date','Due_Date','Save_Date','Submit_Date','Approved_Date','Status','Status'-->
 			<thead>
 				<tr>
-					
+
 					<th align="left">narrative</th>
 					<th align="left">ver_name</th>
 					<th align="left">date_admin</th>
-					
-				
-					
+
+
+
 				</tr>
 			</thead>
 		</table>
 		</div>
-		
+
 		</td></tr>
 		</table>
 <button class="searchbutton" id="adminEditB" onclick="modifyparagraph(event,'ll','33','44');">Edit</button>
