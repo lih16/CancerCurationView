110ec6d336eb5e42f2f0c6bced0f24a933d9f7e4
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-02 11:24:29 -0500

    test

--- view/forgotPassword.php
+++ view/forgotPassword.php
@@ -1,6 +1,6 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link href="<?php echo CSS_PATH; ?>/forgotPassword.css" rel="stylesheet" type="text/css">
+<link href="<?php echo CSS_PATH; ?>/forgotPassword.css" >
 <div class="form-gap"></div>
 <div class="container">
  <div class="row">
