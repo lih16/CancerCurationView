9f290e48b5ffb27b6693d9ddcdb47550ebd974e0
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-02 11:47:51 -0500

    added periods

--- view/forgotPassword.php
+++ view/forgotPassword.php
@@ -1,10 +1,10 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <!--link href="<?php echo CSS_PATH; ?>/forgotPassword.css" -->
-<style>form-gap {
+<style>.form-gap {
     padding-top: 70px;
 }
-container {
+.container {
   /*width: 960px;
     height: 610px;
     margin: 0 auto; */
