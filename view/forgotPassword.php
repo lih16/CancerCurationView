b0a4f290fec5ad03290f99def9cf393c44b70fab
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-05 16:04:56 -0500

    tes

--- view/forgotPassword.php
+++ view/forgotPassword.php
@@ -1,51 +1,49 @@
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<!--link href="<!--?php echo CSS_PATH; ?>/forgotPassword.css" -->
-<style>form-gap {
-    padding-top: 70px;
-}
-.container {
-  /*width: 960px;
-    height: 610px;
-    margin: 0 auto; */
-  font-family: 'Droid Serif', serif;
-    color: black;
-  display: flex;
-  justify-content: center;
-  position: relative;
-}
-</style>
-<div class="form-gap"></div>
-<div class="container">
- <div class="row">
-   <div class="col-md-4 col-md-offset-4">
-           <div class="panel panel-default">
-             <div class="panel-body">
-               <div class="text-center">
-                 <h3><i class="fa fa-lock fa-4x"></i></h3>
-                 <h2 class="text-center">Forgot Password?</h2>
-                 <p>You can reset your password here.</p>
-                 <div class="panel-body">
 
-                   <form id="register-form" role="form" autocomplete="off" class="form" method="post">
+.......<!DOCTYPE html>
+<html>
 
-                     <div class="form-group">
-                       <div class="input-group">
-                         <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
-                         <input id="email" name="email" placeholder="email address" class="form-control"  type="email">
-                       </div>
-                     </div>
-                     <div class="form-group">
-                       <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
-                     </div>
+<head>
+  <title>Reset Password - Cancer Alteration Viewer</title>
+  <meta name="robots" content="noindex, nofollow">
+  <link href="<?php echo CSS_PATH; ?>/register.css" rel="stylesheet" type="text/css">
 
-                     <input type="hidden" class="hide" name="token" id="token" value="">
-                   </form>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
-                 </div>
-               </div>
-             </div>
-           </div>
-         </div>
- </div>
-</div>
+  <script>
+    $(document).ready(function() {
+      $("#register").click(function() {
+
+        var email = $("#email").val();
+
+        if (email == '') {
+          alert("Please provide email");
+          return false;
+        } else {
+          $.post("resetPassword.php", {
+            password1: password
+          }, function(data) {
+            if (data == 'You have Successfully reset your password.....') {
+              $("form")[0].reset();
+            }
+            //alert(data);
+          });
+        }
+      });
+    });
+  </script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="main">
+      <form class="form" method="post" action="../register/submit">
+        <h2> Cancer Alteration Viewer Reset Password</h2>
+        <h3><i class="fa fa-lock fa-4x"></i></h3>
+       <label>Email :</label>
+        <input type="text" name="email" id="email">
+        <button type="su" name="rister" id="regster" value="Regiter">Reset Password</button>
+      </form>
+    </div>
+</body>
+</html>
