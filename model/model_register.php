4df67b066cc4753052e05c4b4ae025cfbbbff1d9
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-10 10:39:37 -0500

    test

--- model/model_register.php
+++ model/model_register.php
@@ -96,7 +96,7 @@ class Register_Model extends model_base
         $username = $username;
         $password = $pass;
 
-        $msg     = "Your temporary password is : "$pass;
+        $msg     =   $pass;
         $headers = "From: cav-notifications@sema4genomics.com";
         // send email
         $mail=mail($username, "CAV Password Reset", $msg, $headers);
