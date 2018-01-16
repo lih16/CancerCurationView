d6188f3b7efdaa5e81e79ff303b2f2e6993fe698
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-16 09:38:16 -0500

    fixed push

--- tools/email_notify_report.sh
+++ tools/email_notify_report.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
+IFS=$'\t'
 declare -A emailnotify
  i=0;
  sendString=""
  emailList="osman.siddiqui@sema4genomics.com,osmosis11@gmail.com,osman.siddiqui11@gmail.com"
  while read cancer gene variant; do
 
-emailnotify[$i]=$gene" "$cancer" "$variant;
+emailnotify[$i]=$cancer" "$gene" "$variant;
 
 echo "aaa"${emailnotify[$i]};
 let i=i+1;
 mysql -h ec2-34-234-146-130.compute-1.amazonaws.com -u siddio01 -pfBNsPQ8YKF4G75vjA3zkzPAJ -D kb_CancerVariant_Curation -bse  "update kb_CancerVariant_Curation.CVC_viewer_editor_report set status=1 where cancer='$cancer' and gene='$gene' and varaint='$variant'"
 done < <(echo "select cancer,gene,varaint from kb_CancerVariant_Curation.CVC_viewer_editor_report where   status=0 group by cancer,gene,varaint" | mysql -h ec2-34-234-146-130.compute-1.amazonaws.com -u siddio01 -pfBNsPQ8YKF4G75vjA3zkzPAJ -D kb_CancerVariant_Curation -N)
             #select view-editor only status=0 using group by to remove the duplication
-
+unset IFS
 for i in "${!emailnotify[@]}"
 do
 sendString=$sendString"\n"${emailnotify[$i]};
