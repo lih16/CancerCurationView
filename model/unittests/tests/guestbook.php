92d6b55fa31d2decaf1a6e6444f4c418c318d510
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-25 12:04:52 -0500

    connections test

---
+++ model/unittests/tests/guestbook.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+
+class MyGuestbookTest extends TestCase
+{
+    use TestCaseTrait;
+
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+    public function getConnection()
+    {
+        $pdo = new PDO('sqlite::memory:');
+        return $this->createDefaultDBConnection($pdo, ':memory:');
+    }
+
+    /**
+     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    public function getDataSet()
+    {
+        return $this->createFlatXMLDataSet(dirname(__FILE__).'/_files/guestbook-seed.xml');
+    }
+}
+?>
