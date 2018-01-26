ee2dc241048cedf55bf82c9d77ebabdd20eb568b
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-26 09:18:58 -0500

    test

--- model/unittests/tests/dbtesting.php
+++ model/unittests/tests/dbtesting.php
@@ -2,8 +2,7 @@
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 
-require 'PHPUnit' . DIRECTORY_SEPARATOR . 'Framework' .
-DIRECTORY_SEPARATOR . 'TestCase' ;
+
 
 class FixtureTestCase extends PHPUnit_Extensions_Database_TestCase {
 
