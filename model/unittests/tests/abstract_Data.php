095f3ff9647568b70357d9697bf3562a8676e549
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-26 11:48:01 -0500

    tests

--- model/unittests/tests/abstract_Data.php
+++ model/unittests/tests/abstract_Data.php
@@ -19,12 +19,10 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         echo $row["cancer"]. $row["gene"]. $row["var"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 $conn->close();
-
-?>
