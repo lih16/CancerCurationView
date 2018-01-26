08f89337452555ffba9b999e43a93405cd9f7b5a
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-26 09:12:57 -0500

    test

---
+++ model/unittests/tests/dbtesting.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+
+class FixtureTestCase extends PHPUnit_Extensions_Database_TestCase {
+
+    public $fixtures = array(
+        'posts',
+        'postmeta',
+        'options'
+    );
+
+}
