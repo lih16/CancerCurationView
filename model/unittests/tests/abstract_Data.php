28fe35ff19b2bf8d7ca5d7275f595297e44ff2d3
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-26 10:59:10 -0500

    tests

---
+++ model/unittests/tests/abstract_Data.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+
+abstract class MyApp_Tests_DatabaseTestCase extends TestCase
+{
+    use TestCaseTrait;
+
+    // only instantiate pdo once for test clean-up/fixture load
+    static private $pdo = null;
+
+    // only instantiate PHPUnit_Extensions_Database_DB_IDatabaseConnection once per test
+    private $conn = null;
+
+    final public function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new PDO('mysql:dbname=CVC_cancer_gene_var_CAV_2;host=ec2-34-234-146-130', 'siddio01', 'fBNsPQ8YKF4G75vjA3zkzPAJ');
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, ':memory:');
+        }
+
+        return $this->conn;
+    }
+}
+?>
--- model/unittests/tests/dbtesting.php
+++ model/unittests/tests/dbtesting.php
@@ -1,14 +1,45 @@
 <?php
-use PHPUnit\Framework\TestCase;
-use PHPUnit\DbUnit\TestCaseTrait;
 
+class Db
+{
+    private static $instance = null;
 
-class FixtureTestCase extends PHPUnit_Extensions_Database_TestCase {
+    /*
+     *Accept a $model instance in the constructor, so the  dependencies can be injected from the outside
+    */
+    private function __construct()
+    {
+    }
 
-    public $fixtures = array(
-        'posts',
-        'postmeta',
-        'options'
-    );
+    /**
+    * Empty clone magic method to prevent duplication.
+    */
+    private function __clone()
+    {
+    }
 
+    /*
+     * @return singleton instance for mysql connection
+     *
+     * @throws PDO exceptionclass [description]
+     *
+     * @access public
+     * @static
+     * @see Db::getInstance,
+    */
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            $hostname = DB_HOST;
+            $username = DB_NAME;
+            $password = DB_PASS;
+            $db_name = DB_USER;
+            try {
+                self::$instance = new PDO("mysql:host=$hostname;dbname=$db_name", $username, $password);
+            } catch (PDOException $e) {
+                write_log($e->getMessage());
+            }
+        }
+        return self::$instance;
+    }
 }
