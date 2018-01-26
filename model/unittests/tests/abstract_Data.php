8197a695600e8764d5cc75ab299d79167e4b963b
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-26 11:48:20 -0500

    new test

--- model/unittests/tests/abstract_Data.php
+++ model/unittests/tests/abstract_Data.php
@@ -2,27 +2,39 @@
 use PHPUnit\Framework\TestCase;
 use PHPUnit\DbUnit\TestCaseTrait;
 
-$servername = "34.234.146.130";
-$username = "siddio01";
-$password = "fBNsPQ8YKF4G75vjA3zkzPAJ";
-$dbname = "kb_CancerVariant_Curation";
+class MySqlGuestbookTest extends TestCase
+{
+    use TestCaseTrait;
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    /**
+     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
+     */
+     public function getConnection()
+         {
+            $servername = "34.234.146.130";
+            $username = "siddio01";
+            $password = "fBNsPQ8YKF4G75vjA3zkzPAJ";
+            $dbname = "kb_CancerVariant_Curation";
+            // Create connection
+            $pdo = new PDO('mysql:...', $user, $password);
+            return $this->createDefaultDBConnection($pdo, $database);
 
-$sql = "SELECT cancer, gene, var FROM CVC_cancer_gene_var_CAV_2";
-$result = $conn->query($sql);
+          }
 
-if ($result->num_rows > 0) {
+          public function sampleQuery()
+          {
+            $sql = "SELECT cancer, gene, var FROM CVC_cancer_gene_var_CAV_2";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
     // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo $row["cancer"]. $row["gene"]. $row["var"]. "<br>";
+        while($row = $result->fetch_assoc()) {
+          echo $row["cancer"]. $row["gene"]. $row["var"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 $conn->close();
+}
+}
+?>
