1510923f9c1010fab0e3a1d9987d7c2713a14b76
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-01-31 14:42:48 -0500

    changed controller login

--- controller/controller_login.php
+++ controller/controller_login.php
@@ -38,7 +38,7 @@ class controller_login extends Controller_base
           break;
           case 3 : header("location: ../home/browse");
           break;
-          case 4: include VIEW_PATH . 'data_menu.php'; //'addAlteration.php'
+          case 4: include VIEW_PATH . 'addAlteration.php' ; //'addAlteration.php' 'data_menu.php'
           break;
           default: include VIEW_PATH . 'login.php';
 
