0ca0d92a78c49a99c3ec451be40c9439e505b8f2
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-02-01 15:45:05 -0500

    test

--- view/addPreNarrative.php
+++ view/addPreNarrative.php
@@ -3,9 +3,11 @@
 
 <head>
   <title>Add Narrative - Cancer Alteration Viewer</title>
-  <meta name="robots" content="noindex, nofollow">
-  <link href="<?php echo CSS_PATH; ?>/dataManager.css" rel="stylesheet" type="text/css">
-
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
 
