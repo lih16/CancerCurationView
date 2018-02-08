1b81899d9f403479b1a5c2de74efc603b0a46675
Osman Siddiqui <31106676+osman-siddiqui@users.noreply.github.com>
2018-02-08 12:21:18 -0500

    constructhtml parse error

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -216,7 +216,7 @@ function constructHtml(groupObj) {
 
         for (var i = 0; i < mutations.length; i++) {
             var mutations_w = notifyNarrativeTable(mutations[i]);
-             if (mutations_w == "parse error"){
+             if (mutations_w == "constructhtml parse error"){
                continue;
              }
             constructHtml = constructHtml + "<option>";
